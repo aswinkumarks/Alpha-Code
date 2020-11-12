c600e9a6e6b816e68021e5e26f3117e35beca8f3
aswinkumarks21 <aswinkumarks21@gmail.com>
2020-11-13 01:25:45 +0530

    contest views cleanup

--- AlphaCodeServer/contest/urls.py
+++ AlphaCodeServer/contest/urls.py
@@ -17,7 +17,7 @@ urlpatterns = [
     path('create_contest/<cname>/create_question/', views.create_question),
     path('finalSubmit/<cname>', views.result_pg),
     path('thankyou', views.thankyou_pg),
-    path('testing', views.testing_pg),
+    path('error', views.error_page),
     path('deleteContest/<cname>', views.delete_contest),
     # path('editContest', views.edit_contest)
 ]
